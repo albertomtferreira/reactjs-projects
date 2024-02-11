b8d5d3ffc97cbb9710a2bab21299cc91b4301c35
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2024-02-11 22:24:34 +0000

    star rating
    star rating component and styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Accordian from './components/accordian';
 import ColorGenerator from './components/color_generator/color_generator.component';
+import StarRating from './components/star_rating/star_rating.component';
 
 function App() {
   return (
     <div className="App">
-      {/* Accordian Component       */}
-      {/* <Accordian/> */}
+      {/* Star Rating Component */}
+      <StarRating numberOfStars={10}/>
+
       {/* Color Generator Component */}
       <ColorGenerator/>
+
+      {/* Accordian Component       */}
+      <Accordian/>
     </div>
   );
 }
