8f51b1e2c53fc3e324a154184267963696bb88b4
Alberto Ferreira <albertomtferreira@gmail.com>
2024-02-13 15:55:25 +0000

    qr-code-generator
    create component and styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordian from './components/accordian/accordian.component';
 import ColorGenerator from './components/color_generator/color_generator.component';
 import ImageSlider from './components/image_slider/image_slider.component';
 import LoadMoreButton from './components/load_more_button/load_more_button.component';
+import QrCodeComponent from './components/qr_code_generator/qr_code_generator.component';
 import StarRating from './components/star_rating/star_rating.component';
 import menus from './components/tree_view/data';
 import TreeView from './components/tree_view/tree_view.component';
@@ -10,6 +11,9 @@ import TreeView from './components/tree_view/tree_view.component';
 function App() {
   return (
     <div className="App">
+      {/* QR Code Generator Component */}
+      <QrCodeComponent />
+
       {/* Tree View Component */}
       <TreeView menus={menus} />
 
---
+++ src/components/qr_code_generator/qr_code_generator.component.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./qr_code_component.styles.css";
+import QRCode from "react-qr-code";
+
+export default function QrCodeComponent() {
+  // State management
+  const [qrCode, setQrCode] = useState("QR CODE GENERATOR");
+  const [input, setInput] = useState("");
+  // Function to handle the onClick status from the Generate button
+  function handleGenerateQrCode() {
+    setQrCode(input);
+    setInput("");
+  }
+  return (
+    <div className="qr_code_container">
+      <h1>QR CODE GENERATOR</h1>
+      <div className="qr_code_input">
+        {/* INPUT */}
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          name="qr-code"
+          value={input}
+          placeholder="Enter your value here"
+        />
+        {/* SUBMIT */}
+        <button
+          disabled={input && input.trim() !== "" ? false : true}
+          onClick={handleGenerateQrCode}
+        >
+          Generate
+        </button>
+      </div>
+      <div className="qr_code_generator">
+        {/* QR CODE GENERATOR */}
+        <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+      </div>
+    </div>
+  );
+}
